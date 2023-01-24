064161242c9f0d8cb2e3624cace4dca3212c01a1
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-24 06:27:22 +0100

    Create simple counter app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    console.clear();
+    console.log('Constructor called');
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div className="App"></div>;
+    console.log('Render called');
+
+    return (
+      <div className="App">
+        <div className="counter">Counter: {this.state.counter}</div>
+        <div className="buttons">
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+        </div>
+      </div>
+    );
   }
 }
 
